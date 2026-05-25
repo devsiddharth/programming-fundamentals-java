2b73221197b450b668dac15a98abe2c16e608929
Siddhartha Gajjela <116182685+devsiddharth@users.noreply.github.com>
2026-05-25 19:02:25 +0530

    ✅ Add solution for: Using Compound Assignment Operators

---
+++ exercises/UsingCompoundAssignmentOperators.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+public class Solution { 
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in); 
+        
+        // Write your code here to apply the compound assignment 
+        //operators 
+
+        int initialAmount = sc.nextInt();
+        int addition = sc.nextInt();
+        int subtraction = sc.nextInt();
+
+        System.out.println( initialAmount+addition - subtraction);
+
+        
+        sc.close(); 
+    } 
+}
